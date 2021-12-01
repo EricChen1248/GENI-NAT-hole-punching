af2224e726d50adb624a2f93b9d9f574f48de263
Eric Chen <ericjyc@bu.edu>
2021-12-01 14:59:08 -0500

    setup direct communications for tcp
    Signed-off-by: Eric Chen <ericjyc@bu.edu>

--- client.py
+++ client.py
@@ -25,11 +25,17 @@ def create_udp_sock(port: int) -> socket.socket:
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
     sock.bind(("0.0.0.0", port))
     return sock
+
+def create_tcp_sock(port: int) -> socket.socket:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(("0.0.0.0", port))
+    return sock
             
 target_ip = None
 target_port = None
 
-def setup_direct() -> socket.socket:
+def setup_direct_udp() -> socket.socket:
     global target_ip, target_port, input
     if client == 1:
         target_ip = "10.10.4.2"
@@ -40,8 +46,7 @@ def setup_direct() -> socket.socket:
 
     input = f"direct,{input}".encode()
     
-    if protocol == "udp":
-        sock = create_udp_sock(5006 if client == 1 else 5005)
+    sock = create_udp_sock(5006 if client == 1 else 5005)
 
     connected = False
     while not connected:
@@ -58,6 +63,35 @@ def setup_direct() -> socket.socket:
         connected = True
 
     return sock
+
+def setup_direct_tcp() -> socket.socket:
+    global target_ip, target_port, input
+    if client == 1:
+        target_ip = "10.10.4.2"
+        target_port = 6005
+    else:
+        target_ip = "10.10.4.1"
+        target_port = 6006
+
+    input = f"direct,{input}".encode()
+    
+    sock = create_tcp_sock(6006 if client == 1 else 6005)
+
+    connected = False
+    while not connected:
+        if client == 1:
+            sock.connect((target_ip, target_port))
+            sock.send(str(client).encode())
+            _ = sock.recv(1024)
+        else:
+            sock.listen(1)
+            conn, _ = sock.accept()
+            _ = conn.recv(1024)
+            conn.send(str(client).encode())
+            sock = conn
+        connected = True
+    return sock
+
 
 def setup_relay():
     global target_ip, target_port, input
@@ -120,13 +154,21 @@ def setup_punch() -> socket.socket:
 def send_round_trip_message(sock: socket.socket, message: bytes = b'') -> int:
     if client == 1: 
         start = time.time()
-        sock.sendto(message , (target_ip, target_port))
-        sock.recvfrom(1024)
+        if protocol == 'udp':
+            sock.sendto(message , (target_ip, target_port))
+            sock.recvfrom(1024)
+        elif protocol == 'tcp':
+            sock.send(message)
+            sock.recv(1024)
         end = time.time()
         return end - start
     else:
-        _, addr = sock.recvfrom(1024)
-        sock.sendto(input, addr)
+        if protocol == 'udp':
+            _, addr = sock.recvfrom(1024)
+            sock.sendto(input , addr)
+        elif protocol == 'tcp':
+            sock.recv(1024)
+            sock.send(input)
         return None
 
 def communicate(sock: socket.socket):
@@ -142,13 +184,12 @@ def communicate(sock: socket.socket):
         print("Average:", total_time / count)
 
 
-
-
-
-
 def main():
     if mode == 'direct':
-        sock = setup_direct()
+        if protocol == "udp":
+            sock = setup_direct_udp()
+        elif protocol == "tcp":
+            sock = setup_direct_tcp()
     if mode == 'relay':
         sock = setup_relay()
     if mode == 'punch':
